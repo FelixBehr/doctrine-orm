e70912486e63f9e49dfe70e6ba7bf894587249c3
Matthias Pigulla <mp@webfactory.de>
2023-01-24 20:28:32 +0000

    Review the conditions how/when the Mapping Driver shall skip fields

--- lib/Doctrine/ORM/Mapping/Driver/AnnotationDriver.php
+++ lib/Doctrine/ORM/Mapping/Driver/AnnotationDriver.php
@@ -344,14 +344,36 @@ class AnnotationDriver extends CompatibilityAnnotationDriver
 
         // Evaluate annotations on properties/fields
         foreach ($class->getProperties() as $property) {
+            // Handle the case that reflection may report properties inherited from parent classes.
+            // When we know about the fields already (inheritance has been anticipanted in ClassMetadataFactory),
+            // skip them.
+            // The declaring classes may mismatch when there are private properties: The same property name may be
+            // reported multiple times, but since it is private, it is in fact multiple (different) properties in
+            // different classes. In that case, report the property as an individual field. (ClassMetadataFactory will
+            // probably fail in that case, though.)
+
+            $declaringClass = $property->getDeclaringClass()->getName();
+
             if (
-                $metadata->isMappedSuperclass && ! $property->isPrivate()
-                ||
-                $metadata->isInheritedField($property->name)
-                ||
-                $metadata->isInheritedAssociation($property->name)
-                ||
-                $metadata->isInheritedEmbeddedClass($property->name)
+                isset($metadata->fieldMappings[$property->name])
+                && isset($metadata->fieldMappings[$property->name]['declared'])
+                && $metadata->fieldMappings[$property->name]['declared'] === $declaringClass
+            ) {
+                continue;
+            }
+
+            if (
+                isset($metadata->associationMappings[$property->name])
+                && isset($metadata->associationMappings[$property->name]['declared'])
+                && $metadata->associationMappings[$property->name]['declared'] === $declaringClass
+            ) {
+                continue;
+            }
+
+            if (
+                isset($metadata->embeddedClasses[$property->name])
+                && isset($metadata->embeddedClasses[$property->name]['declared'])
+                && $metadata->embeddedClasses[$property->name]['declared'] === $declaringClass
             ) {
                 continue;
             }
